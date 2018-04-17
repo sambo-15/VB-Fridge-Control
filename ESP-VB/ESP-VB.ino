95232d8ef68e3d1eae362b1f48693812d46b387d
Sambo-15 <gabe.sampson1505@gmail.com>
2018-04-17 15:22:54 +1000

    Removed Comments

--- ESP-VB/ESP-VB.ino
+++ ESP-VB/ESP-VB.ino
@@ -1,31 +1,29 @@
 /*
  Board: WeMos D1 MINI (https://tronixlabs.com.au/arduino/boards/wireless/wemos-d1-mini-arduino-compatible-esp8266-wifi-australia/)
  Shield: https://tronixlabs.com.au/esp8266/wemos-esp8266/wemos-d1-mini-relay-shield-v2-australia/
+ 
  Libraries: 
-  ESP8266Wifi (Add via Additional Boards Link): http://www.instructables.com/id/Wemos-ESP8266-Getting-Started-Guide-Wemos-101/
-  PubSubClient (Add via Library Manager): https://github.com/knolleary/pubsubclient
+ ESP8266Wifi (Add via Additional Boards Link): http://www.instructables.com/id/Wemos-ESP8266-Getting-Started-Guide-Wemos-101/
+ PubSubClient (Add via Library Manager): https://github.com/knolleary/pubsubclient
 
- Whacked together by James Rule and Ethan Dunne for the MakerSpaceIoT 101 Workshop
+ Made by Gabe Sampson and Friends
  */
  
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-// Update these with values suitable for your network
 const char* ssid = "iAccelerate";
-const char* password = "AcceLer47eIC$";
-// The MQTT Server you want to use, this is https://test.mosquitto.org/ 
+const char* password = "";
 const char* mqtt_server = "test.mosquitto.org"; 
 
-// Variables for the MQTT Library
 WiFiClient espClient;
 PubSubClient client(espClient);
 long lastMsg = 0;
 char msg[50];
 int value = 0;
 
-const int relayPin = D1; // For the WeMos D1 Mini Relay Shield
-const long interval = 500; // The delay ensures the relay cannot be triggered too quickly 
+const int relayPin = D1; 
+const long interval = 500; 
 const int ledPin = BUILTIN_LED;
 
 void setup() {
@@ -33,9 +31,9 @@ void setup() {
         setup_wifi();
         client.setServer(mqtt_server, 1883);
         pinMode(relayPin, OUTPUT);
-        pinMode(ledPin, OUTPUT);  // set initial state, LED OFF
-        digitalWrite(ledPin, HIGH); // LED Off
-        client.setCallback(callback); // The callback will trigger the relay when a message arrives
+        pinMode(ledPin, OUTPUT);  
+        digitalWrite(ledPin, HIGH); 
+        client.setCallback(callback); 
 }
 
 void setup_wifi() { 
@@ -80,12 +78,12 @@ void callback(char* topic, byte* payload, unsigned int length) {
         }
         Serial.println();        
         if ((char)payload[0] == '1') {        
-                digitalWrite(relayPin, HIGH); // Turn ON relay with voltage HIGH
-                digitalWrite(ledPin, LOW); // LED On
+                digitalWrite(relayPin, HIGH); 
+                digitalWrite(ledPin, LOW);
                 delay(interval);   
         } else {
-                digitalWrite(relayPin, LOW);  // Turn OFF relay with voltage LOW
-                digitalWrite(ledPin, HIGH); // LED Off
+                digitalWrite(relayPin, LOW);  
+                digitalWrite(ledPin, HIGH);
                 delay(interval);
         }
 }
